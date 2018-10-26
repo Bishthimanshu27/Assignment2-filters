f67822d48a188a28bfad8f617dd4ff3cd1fa56ef
Himanshu Bisht <Himanshubisht227@gmail.com>
2018-10-26 11:23:23 -0500

    done

--- Assignment2inclass/Controllers/HomeController.cs
+++ Assignment2inclass/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assignment2inclass.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace Assignment2inclass.Controllers
 {
     public class HomeController : Controller
     {
+        [CrawlerActionFilter]
         public ActionResult Index()
         {
             return View();
---
+++ Assignment2inclass/Helpers/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment2inclass.Helpers
+{
+    public static class Extensions
+    {
+        public static int ToAge(this DateTime age, DateTime birthday)
+        {
+            int difference = birthday.Month - age.Month;
+            if (difference < 0)
+            {
+                difference = difference / 365;
+            }
+            return difference;
+        }
+
+        public static string ToDigitsOnly(this string Digit)
+        {
+            var Number = "";
+            foreach (var n in Digit)
+            {
+                if (char.IsDigit(n))
+                {
+                    Number += n;
+                }
+            }
+            return Number;
+        }
+    }
+}
