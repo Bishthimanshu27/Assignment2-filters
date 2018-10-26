3b758c3515c6cf3d060206cf55a7fc17e8dafd12
Himanshu Bisht <Himanshubisht227@gmail.com>
2018-10-26 09:42:56 -0500

    Firstcommit

---
+++ Assignment2inclass/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment2inclass.Startup))]
+namespace Assignment2inclass
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
